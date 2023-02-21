e1e608909bbc2cea1d59125cb9f1a3c1b00d9ff0
keith-gamble <keith.gamble@bwdesigngroup.com>
2023-02-21 08:22:39 -0800

    Cleaned up and reorganized project

--- common/src/main/java/com/bwdesigngroup/ignition/tag_cicd/common/WebUtilities.java
+++ common/src/main/java/com/bwdesigngroup/ignition/tag_cicd/common/WebUtilities.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Keith Gamble
+ * Copyright 2023 Barry-Wehmiller Design Group
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -8,9 +8,6 @@ package com.bwdesigngroup.ignition.tag_cicd.common;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.inductiveautomation.ignition.common.gson.JsonObject;
 
 /**
@@ -18,7 +15,6 @@ import com.inductiveautomation.ignition.common.gson.JsonObject;
  * @author Keith Gamble
  */
 public class WebUtilities {
-	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	/**
 	 * Returns a jsonObject to represent an HTTP error of status 400.
